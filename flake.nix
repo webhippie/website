6d7909d526b0dae92bb34b85f962def026332325
Thomas Boerger <thomas@webhippie.de>
2025-09-11 16:13:52 +0200

    chore: update flake config

--- flake.nix
+++ flake.nix
@@ -10,20 +10,21 @@
       url = "github:cachix/devenv";
     };
 
-    pre-commit-hooks-nix = {
-      url = "github:cachix/pre-commit-hooks.nix";
-    };
-
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
-        inputs.pre-commit-hooks-nix.flakeModule
+        inputs.git-hooks.flakeModule
       ];
 
       systems = [
@@ -33,46 +34,51 @@
         "aarch64-darwin"
       ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        imports = [
-          {
-            _module.args.pkgs = import inputs.nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          }
-        ];
-
-        pre-commit = {
-          settings = {
-            hooks = {
-              nixpkgs-fmt = {
-                enable = true;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          imports = [
+            {
+              _module.args.pkgs = import inputs.nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
               };
-              golangci-lint = {
-                enable = true;
-              };
-            };
-          };
-        };
+            }
+          ];
 
-        devenv = {
-          shells = {
-            default = {
-              languages = {
-                javascript = {
-                  enable = true;
-                  package = pkgs.nodejs_20;
+          devenv = {
+            shells = {
+              default = {
+                git-hooks = {
+                  hooks = {
+                    nixfmt-rfc-style = {
+                      enable = true;
+                    };
+                  };
                 };
-              };
 
-              packages = with pkgs; [
-                gnumake
-                hugo
-              ];
+                languages = {
+                  javascript = {
+                    enable = true;
+                    package = pkgs.nodejs_20;
+                  };
+                };
+
+                packages = with pkgs; [
+                  gnumake
+                  hugo
+                  nixfmt-rfc-style
+                ];
+              };
             };
           };
         };
-      };
     };
 }
